8bf9e3f06ddb0e92db50d4b022b32fa3f533f33a
yara <yaraeb121@gmail.com>
2021-01-06 01:36:45 +0200

    staged

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./App.css";
+import Video from "./Video";
+import movie from "./movie.mp4";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+      return response;
+    }
+    fetchPosts();
+  }, []);
+  console.log(videos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
+        {/* <Video
+          url={movie}
+          channel="eetttyyy"
+          description="Yoo this works"
+          song="99 problems but REACT AINT ONE!"
+          likes={123}
+          messages={456}
+          shares={789}
+        />
+        <Video
+          url={movie}
+          channel="eetttyyy"
+          description="Yoo this works"
+          song="99 problems but REACT AINT ONE!"
+          likes={123}
+          messages={456}
+          shares={789}
+        /> */}
+      </div>
+
+      {/* app container */}
+      {/* videos */}
     </div>
   );
 }
